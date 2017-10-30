b5a49b63bd72645e65779de2673b6039cf0fe312
Vitor Menezello <vitormenezello@gmail.com>
2017-10-29 22:00:19 -0200

    Update Op.java

--- src/Inter/Op.java
+++ src/Inter/Op.java
@@ -4,7 +4,10 @@ import Lexer.*;
 import Symbols.*;
 
 public class Op extends Expr {
-    public Op(Token tok, Type p) { super(tok, p); }
+    public Op(Token tok, Type p) {
+        super(tok, p);
+    }
+    
     public Expr reduce() {
         Expr x = gen();
         Temp t = new Temp(type);
